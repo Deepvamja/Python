6caddf3d43f4c5e1168817525fb60759b2664fb2
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2024-12-09 10:42:53 +0530

    Add files via upload

---
+++ conditional expression.py
@@ -0,0 +1,17 @@
+a = int(input('enter your age:'))
+
+# if elif else ladder
+
+if(a>=18):
+    print("your are above age 18")
+
+elif(a<0):
+    print('invalid age')
+
+elif(a==0):
+    print('invalid 0 is not valid age')
+
+else:
+    print("you are below age 18")
+
+print('end here')

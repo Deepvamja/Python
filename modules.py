2dfa667dfb2be055d298f8b00524bdf411bb4a8b
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2025-01-26 11:33:12 +0530

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,27 @@
+# ''' module = a file containing code you want to include in your program
+#             use 'import' to include a module (built-in or your own)'''
+
+# list of modules
+
+# print(help('modules'))
+
+# #method-1
+# import math
+# #method-2
+# #  import math as m (as alias)
+# #method-3
+# #   from math import pi (not as recommended)
+
+# print(math.pi)
+
+# #print(m.pi)
+
+# #print(pi)
+
+import module_example
+
+result = module_example
+result = module_example.square(9)
+
+print(result)
+

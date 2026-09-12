81272822a40cf28245d16cb7b0c166adeb48e5c0
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2026-09-12 10:59:02 +0530

    Update CreateLongMultiLineString.py

--- CreateLongMultiLineString.py
+++ CreateLongMultiLineString.py
@@ -3,3 +3,10 @@ learn to program is
 by writing code.'''
 
 print(my_string)
+
+
+my_string = "The only way to \n" \
+        	"learn to program is \n" \
+        	"by writing code."
+
+print(my_string)

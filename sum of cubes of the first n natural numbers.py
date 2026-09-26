05f78475e555ddaf291f784bb17b0e82416d1f1b
deepvamja <143236575+Deepvamja@users.noreply.github.com>
2026-09-26 09:28:01 +0530

    Create sum of cubes of the first n natural numbers.py

---
+++ sum of cubes of the first n natural numbers.py
@@ -0,0 +1,17 @@
+n = 5
+res = ((n * (n + 1)) // 2) ** 2
+print(res)
+
+
+n = 5
+sum = 0
+
+for i in range(1, n + 1):
+    res += i ** 3
+
+print(res)
+
+
+n = 5
+res = sum([(i+1) ** 3 for i, _ in enumerate(range(n))])
+print(res)
